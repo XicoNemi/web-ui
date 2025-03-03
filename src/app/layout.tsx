686b1fd7d249567fe14531085132fb9130a01df8
Haziel Ortiz <eortiz@inncol.com.mx>
2025-03-03 00:26:28 -0600

    feat(seo): increase seo for application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,49 @@ import ProviderWrapper from '@/components/providers/ProviderWrapper';
 // Metadata
 export const metadata: Metadata = {
   title: 'XicoNemi | Dashboard',
+  description: 'XicoNemi, el mejor software para gestión de negocios y turismo',
+  authors: [{ name: 'Dream Team' }],
+  category: 'Administración de negocios y turismo',
+  applicationName: 'XicoNemi - Dashboard',
+  keywords: ['xicotepec', 'puebla', 'mexico', 'turismo', 'pueblos magicos'],
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/icons/maskable.png',
+    apple: '/images/apple-touch-icon.png',
+  },
+  openGraph: {
+    title: 'XicoNemi - Impulsa tu negocio en los pueblos mágicos',
+    description:
+      'Con XicoNemi, podrás gestionar tu negocio o evento de manera eficiente y sencilla. ¡Descubre cómo podemos ayudarte a crecer!',
+    url: 'https://pos.kornu.mx',
+    type: 'website',
+    images: [
+      {
+        url: '/images/seo/dashboard.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'XicoNemi - Vista general de nuestro sistema',
+      },
+    ],
+    siteName: 'XicoNemi',
+    locale: 'es_MX',
+  },
+  twitter: {
+    creator: '@XicoNemi',
+    creatorId: 'id',
+    card: 'summary_large_image',
+    title: 'XicoNemi - Impulsa tu negocio en los pueblos mágicos',
+    description:
+      'Con XicoNemi, podrás gestionar tu negocio o evento de manera eficiente y sencilla. ¡Descubre cómo podemos ayudarte a crecer!',
+    images: [
+      {
+        url: '/images/seo/dashboard.jpg',
+        alt: 'XicoNemi - Vista general de nuestro sistema',
+      },
+    ],
+  },
+
+  robots: 'index, follow',
 };
 
 interface RootLayoutProps {
